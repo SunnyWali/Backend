19f70c529b5148302006183cb3d7b480b7ea508b
Sunny Wali <sunnywali777@gmail.com>
2024-10-29 17:12:54 +0530

    created a instagram2EJS directory

---
+++ ejsDir/instagram2EJS/index.js
@@ -0,0 +1,27 @@
+const express=require("express");
+const app=express();
+const path=require("path");
+const port=8080;
+
+app.listen(port,()=>
+{
+    console.log("Server is listening to the client request");
+});
+
+app.set("view engine","ejs");
+app.set("views",path.join(__dirname,"/views"));
+
+app.get("/:username",(req,res)=>
+{
+    let{username}=req.params;
+    const data=require("./data.json");
+    const userData=data[username];
+    if(userData)
+    {
+    res.render("instagram",{userData});
+    }
+    else
+    {
+        res.render("error");
+    }
+})
