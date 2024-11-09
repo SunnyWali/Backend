28b23342de61c7a9c37eb6fd036a59fd07b218d0
Sunny Wali <sunnywali777@gmail.com>
2024-11-09 11:58:44 +0530

    changed made in the restFulApis

--- restFulApis/index.js
+++ restFulApis/index.js
@@ -2,7 +2,9 @@ const express=require("express");
 const app=express();
 const port=8080;
 const path=require("path");
-const { ppid } = require("process");
+// const { ppid } = require("process");
+const { v4: uuidv4 } = require('uuid');
+uuidv4(); // ⇨ '1b9d6bcd-bbfd-4b2d-9b5d-ab8dfbbd4bed'
 app.listen(port,()=>
 {
     console.log("Server is listening to the port no: 8080");
@@ -14,17 +16,17 @@ app.use(express.urlencoded({extended:true}));
 app.use(express.static(path.join(__dirname,"public")));
 let posts=[
     {
-        id:"1a",
+        id:uuidv4(),
         username:"Sunny Wali",
         content:"I am from Jammu and Kashmir and i belong to kashmiri pandit group",
     },
     {
-        id:"2b",
+        id:uuidv4(),
         username:"Krishna Yadav",
         content:"I am from bihar and i am a full stack developer",
     },
     {
-        id:"3c",
+        id:uuidv4(),
         username:"Ramesh Pellivella",
         content:"I am from hyderabad and i am a backend developer",
     },
@@ -47,10 +49,11 @@ app.get("/posts/new",(req,res)=>
 
 app.post("/posts",(req,res)=>
 {
+    let id=uuidv4();
     let{username,content}=req.body;
     // res.send("Server is working on post request");
     console.log("Server is working on post request");
-    posts.push({username,content});
+    posts.push({id,username,content});
     res.redirect("/posts");
 });
 
