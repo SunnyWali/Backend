b576ea437eed43927d04c9cdcdd159f304b69922
Sunny Wali <sunnywali777@gmail.com>
2024-11-09 16:07:05 +0530

    added some functionality in restFulApis

--- restFulApis/index.js
+++ restFulApis/index.js
@@ -5,15 +5,13 @@ const path=require("path");
 // const { ppid } = require("process");
 const { v4: uuidv4 } = require('uuid');
 uuidv4(); // ⇨ '1b9d6bcd-bbfd-4b2d-9b5d-ab8dfbbd4bed'
-app.listen(port,()=>
-{
-    console.log("Server is listening to the port no: 8080");
-});
+const methodOverride=require("method-override");
 
 app.set("view engine","ejs");
 app.set("views",path.join(__dirname,"views"));
 app.use(express.urlencoded({extended:true}));
 app.use(express.static(path.join(__dirname,"public")));
+app.use(methodOverride("_method"));
 let posts=[
     {
         id:uuidv4(),
@@ -63,3 +61,26 @@ app.get("/posts/:id",(req,res)=>
     let post=posts.find((p)=>id===p.id);
     res.render("show",{post});
 });
+
+app.get("/posts/:id/edit",(req,res)=>
+{
+    let{id}=req.params;
+    let post=posts.find((p)=>id===p.id);
+    res.render("update",{post});
+});
+
+app.patch("/posts/:id",(req,res)=>
+{
+    let{id}=req.params;
+    let post=posts.find((p)=>id===p.id);
+    let newContent=req.body.content;
+    post.content=newContent;
+    res.redirect("/posts");
+});
+
+
+
+app.listen(port,()=>
+    {
+        console.log("Server is listening to the port no: 8080");
+    });
