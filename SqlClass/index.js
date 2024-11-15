ca0a1bc918aac61f901b46308d9ec11ebe6f870b
Sunny Wali <sunnywali777@gmail.com>
2024-11-15 15:20:07 +0530

    insert data into tables

--- SqlClass/index.js
+++ SqlClass/index.js
@@ -5,10 +5,15 @@ const connection = mySql.createConnection({
     host: 'localhost',
     user: 'root',
     password: 'Sunnylee@123',
-    database: 'delta_app'
+    database: 'sunny'
 });
+let q="Insert into user1(id,username,email,password) values ?";
+let users=[
+    ["101","sunny","sunny@gmail.com","sunny123"],
+    ["102","ramesh","ramesh@gmail.com","ramesh123"]
+];
 try {
-    connection.query("Show Tables", (err, result) => {
+    connection.query(q,[users], (err, result) => {
         if (err) throw err;
         console.log(result);
     });
@@ -18,13 +23,13 @@ catch(err)
     console.log(err);
 }
 connection.end(); 
-let getRandomData = () => {
-    return {
-        userId: faker.string.uuid(),
-        username: faker.internet.username(), // before version 9.1.0, use userName()
-        email: faker.internet.email(),
-        password: faker.internet.password(),
-    };
-}
+// let getRandomData = () => {
+//     return {
+//         userId: faker.string.uuid(),
+//         username: faker.internet.username(), // before version 9.1.0, use userName()
+//         email: faker.internet.email(),
+//         password: faker.internet.password(),
+//     };
+// }
 
-console.log(getRandomData());
+// console.log(getRandomData());
--- SqlClass/schema.sql
+++ SqlClass/schema.sql
@@ -1,1 +1,6 @@
-SHOW TABLES;
+create table user1(
+    id int primary key,
+    username varchar(60) unique,
+    email varchar(60) unique not null,
+    password varchar(60) unique
+);
