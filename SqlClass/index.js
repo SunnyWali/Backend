b346dfc7b0d65c6a7c58df12c2bd264a40cbaa19
Sunny Wali <sunnywali777@gmail.com>
2024-11-15 13:08:31 +0530

    changes

--- SqlClass/index.js
+++ SqlClass/index.js
@@ -1,12 +1,30 @@
 const { faker } = require('@faker-js/faker');
+const mySql = require("mysql2");
 
-let getRandomData=()=> {
-  return {
-    userId: faker.string.uuid(),
-    username: faker.internet.username(), // before version 9.1.0, use userName()
-    email: faker.internet.email(),
-    password: faker.internet.password(),
-  };
+const connection = mySql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    password: 'Sunnylee@123',
+    database: 'delta_app'
+});
+try {
+    connection.query("Show Tables", (err, result) => {
+        if (err) throw err;
+        console.log(result);
+    });
+}
+catch(err)
+{
+    console.log(err);
+}
+connection.end(); 
+let getRandomData = () => {
+    return {
+        userId: faker.string.uuid(),
+        username: faker.internet.username(), // before version 9.1.0, use userName()
+        email: faker.internet.email(),
+        password: faker.internet.password(),
+    };
 }
 
 console.log(getRandomData());
