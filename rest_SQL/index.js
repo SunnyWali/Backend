54faf2af8a015f56239d89ae16b2be64b3a34bb3
Sunny Wali <sunnywali777@gmail.com>
2024-11-18 13:24:13 +0530

    made some changes

--- rest_SQL/index.js
+++ rest_SQL/index.js
@@ -3,9 +3,9 @@ const app = express();
 const mysql = require("mysql2");
 const { faker } = require('@faker-js/faker');
 const path = require("path");
-const methodOverride=require("method-override");
+const methodOverride = require("method-override");
 app.use(methodOverride('_method'));
-app.use(express.urlencoded({extended:true}));
+app.use(express.urlencoded({ extended: true }));
 app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "views"));
 const connection = mysql.createConnection({
@@ -32,58 +32,50 @@ app.get("/", (req, res) => {
 
 //Home Route
 app.get("/users", (req, res) => {
-    let q="Select * from users";
-try {
-    connection.query(q,(err,users)=>
-    {
-        if(err) throw err;
-        res.render("show",{users});
-    });
-} catch (err) {
-    res.send("error in db !");
-}
+    let q = "Select * from users";
+    try {
+        connection.query(q, (err, users) => {
+            if (err) throw err;
+            res.render("show", { users });
+        });
+    } catch (err) {
+        res.send("error in db !");
+    }
 });
 
 //Edit Route
-app.get("/users/:id/edit",(req,res)=>
-{
-    let {id}=req.params;
-    let q=`Select * from users where id ='${id}'`;
-    try{
-        connection.query(q,(err,result)=>
-        {
-            if(err) throw err;
-            let user=result[0];
-            res.render("update",{user});
+app.get("/users/:id/edit", (req, res) => {
+    let { id } = req.params;
+    let q = `Select * from users where id ='${id}'`;
+    try {
+        connection.query(q, (err, result) => {
+            if (err) throw err;
+            let user = result[0];
+            res.render("update", { user });
         });
     }
-    catch(err)
-    {
+    catch (err) {
         console.log(err);
     }
 });
 
 //Update Route
-app.patch("/users/:id",(req,res)=>
-{
-    let{id}=req.params;
-    let{name:newUsername,passsword:formpassword}=req.body;
-    let q=`Select * from users where id='${id}'`;
+app.patch("/users/:id", (req, res) => {
+    let { id } = req.params;
+    let { name: newUsername, passsword: formpassword } = req.body;
+    let q = `Select * from users where id='${id}'`;
     try {
-        connection.query(q,(err,result)=>
-        {
-            let user=result[0];
-            if(err) throw err;
-            if(formpassword!=user.passsword)
-            {
+        connection.query(q, (err, result) => {
+            let user = result[0];
+            if (err) throw err;
+            if (formpassword != user.passsword) {
                 res.send('Incorrect Password');
             }
-            else
-            {
+            else {
                 try {
-                    let q=`Update users set name='${newUsername}' where id='${id}'`;
-                    connection.query(q,(err,result)=>{
-                        if(err) throw err;
+                    let q = `Update users set name='${newUsername}' where id='${id}'`;
+                    connection.query(q, (err, result) => {
+                        if (err) throw err;
                         res.redirect("/users");
                     })
                 } catch (err) {
@@ -95,25 +87,36 @@ app.patch("/users/:id",(req,res)=>
         console.log(err);
     }
 });
-app.get("/users/new",(req,res)=>
-{
+app.get("/users/new", (req, res) => {
     res.render("new");
 });
 
-app.post("/users",(req,res)=>
-{
-    let id=faker.string.uuid();
-    let{name,email,passsword}=req.body;
-    let q=`Insert into users(id,name,email,passsword) values('${id}','${name}','${email}','${passsword}')`;
+app.post("/users", (req, res) => {
+    let id = faker.string.uuid();
+    let { name, email, passsword } = req.body;
+    let q = `Insert into users(id,name,email,passsword) values('${id}','${name}','${email}','${passsword}')`;
     try {
-        connection.query(q,(err,result)=>
-        {
-            if(err) throw err;
+        connection.query(q, (err, result) => {
+            if (err) throw err;
             res.redirect("/users");
         });
     } catch (err) {
         res.send("failed to insert the data into the database");
     }
+});
+
+app.delete("/users/:id", (req, res) => {
+    let { id } = req.params;
+    let row = `Delete from users where id='${id}'`;
+    try {
+        connection.query(row, (err, result) => {
+            if (err) throw err;
+            res.redirect("/users");
+        });
+    } catch (err) {
+        console.log(err);
+    }
+
 });
 app.listen('8080', (req, res) => {
     console.log("Server is listening to port 8080");
