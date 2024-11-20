7c9bc7096d573eaf24c1aab971d483b7a61cdcb4
Sunny Wali <sunnywali777@gmail.com>
2024-11-20 19:53:37 +0530

    made some changes

---
+++ mongo/index.js
@@ -0,0 +1,15 @@
+const mongoose=require("mongoose");
+
+async function main()
+{
+    await mongoose.connect("mongodb://127.0.0.1:27017/test");
+}
+
+main().then(()=>
+{
+    console.log("connection established successfull");
+})
+.catch((err)=>
+{
+    console.log(err);
+});
