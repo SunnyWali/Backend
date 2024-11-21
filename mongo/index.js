edbc54a4c71f8f4ff107773d089ef0f5cc08d7a0
Sunny Wali <sunnywali777@gmail.com>
2024-11-21 09:13:28 +0530

    made some changes

--- mongo/index.js
+++ mongo/index.js
@@ -1,15 +1,22 @@
 const mongoose=require("mongoose");
 
-async function main()
-{
+async function main(){
     await mongoose.connect("mongodb://127.0.0.1:27017/test");
 }
 
 main().then(()=>
 {
-    console.log("connection established successfull");
+    console.log("successfully connected");
 })
-.catch((err)=>
+.catch(()=>
 {
-    console.log(err);
+    console.log("error in connecting to the mongodb database");
 });
+
+const userSchema=new mongoose.Schema({
+    name:'String',
+    city:'String',
+    age:'Number'
+});
+
+const Employee=mongoose.model("Employee",userSchema);
