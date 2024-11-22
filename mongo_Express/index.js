b948bc692f44eddf4d21b3435d2bd92d41358457
Sunny Wali <sunnywali777@gmail.com>
2024-11-22 09:07:19 +0530

    made some changes

--- mongo_Express/index.js
+++ mongo_Express/index.js
@@ -2,6 +2,10 @@ const express=require("express");
 const app=express();
 const mongoose=require("mongoose");
 const Chat=require("./models/chat.js");
+const path=require("path");
+app.set("view engine","ejs");
+app.set("views",path.join(__dirname,"views"));
+app.use(express.static(path.join(__dirname,"public")));
 async function main(){
     await mongoose.connect("mongodb://127.0.0.1:27017/whatsapp");
 }
@@ -11,13 +15,10 @@ app.listen(8080,()=>{
     console.log("Server is listening to the port no 8080");
 });
 
-let chat1=new Chat({
-    from:"Ramesh",
-    to:'Krishna',
-    msg:'Hello World',
-    created_at:new Date()
+//index route
+app.get("/chats", async(req,res)=>{
+    let chats=await Chat.find();
+    res.render("index",{chats});
 });
-
-chat1.save().then((result)=>{console.log(result);}).catch((err)=>{console.log(err)});
 
 
