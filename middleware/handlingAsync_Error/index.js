466520a8a6b79253a3197a96c4ac00f5d04c9d87
Sunny Wali <sunnywali777@gmail.com>
2024-12-06 13:25:59 +0530

    made some changes

--- middleware/handlingAsync_Error/index.js
+++ middleware/handlingAsync_Error/index.js
@@ -87,6 +87,20 @@ app.delete("/chats/:id", asyncWrap(async (req, res,next) => {
     res.redirect("/chats"); 
 }));
 
+const handleValidationError=(err)=>{
+    console.log("This is a validation error.Please follow rules");
+    console.dir(err.name);
+    return err;
+}
+//Middleware to print the error name
+app.use((err,req,res,next)=>{
+    console.log(err.name);
+    if(err.name==="Validation Error")
+    {
+         err=handleValidatonError(err);
+    }
+    next(err);
+})
 //Error Handling Middleware
 app.use((err, req, res, next) => {
     let { status = 500, message = "Chat not found" } = err;
